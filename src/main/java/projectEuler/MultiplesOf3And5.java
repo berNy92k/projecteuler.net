820b9376c4877e30d41e7ecf3e993009151881b8
daw.bernacki@gmail.com <mirradave13>
2018-06-03 13:40:57 +0200

    MultiplesOf3And5 - done

--- src/main/java/projectEuler/MultiplesOf3And5.java
+++ src/main/java/projectEuler/MultiplesOf3And5.java
@@ -8,5 +8,39 @@ package projectEuler;
 public class MultiplesOf3And5 {
     public static void main(String[] args) {
 
+        System.out.println("Sum of all the multiples of 3 or 5 below 10:");
+        System.out.println(from0to10());
+
+        System.out.println();
+
+        System.out.println("Sum of all the multiples of 3 or 5 below 1000:");
+        System.out.println(from0to1000());
+
+    }
+
+    private static int from0to10() {
+        int n = 0;
+        int m = 10;
+        int sum = 0;
+        while (n < m){
+            if (n%3==0 || n%5==0){
+                sum = sum + n;
+            }
+            n++;
+        }
+        return sum;
+    }
+
+    private static int from0to1000() {
+        int n = 0;
+        int m = 1000;
+        int sum = 0;
+        while (n < m){
+            if (n%3==0 || n%5==0){
+                sum = sum + n;
+            }
+            n++;
+        }
+        return sum;
     }
 }
