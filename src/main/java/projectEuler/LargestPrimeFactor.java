20e80d7e4c9f0a795b462d0035fa9186fa8a4976
daw.bernacki@gmail.com <mirradave13>
2018-06-07 23:36:13 +0200

    LargestPrimeFactor - step1

--- src/main/java/projectEuler/LargestPrimeFactor.java
+++ src/main/java/projectEuler/LargestPrimeFactor.java
@@ -4,8 +4,31 @@ package projectEuler;
 //
 //  What is the largest prime factor of the number 600851475143 ?
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LargestPrimeFactor {
     public static void main(String[] args) {
+
+        long number = 13195;
+        double pierwNumber = Math.sqrt(number);
+        List<Long> liczbyPierwsze = new LinkedList<>();
+
+        for (long i = 1; i <= pierwNumber; i++) {
+            if (i != 1 && number % i == 0) {
+                long k = 0;
+                for (long j = 2; j <= i; j++) {
+                    if (i % j == 0) {
+                        k++;
+                    }
+                }
+                if (k == 1) {
+                    liczbyPierwsze.add(i);
+                }
+            }
+        }
+
+        System.out.println(liczbyPierwsze);
 
     }
 }
