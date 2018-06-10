c58c6872523e04067bceeb8b66a02d889c962428
daw.bernacki@gmail.com <mirradave13>
2018-06-10 03:11:57 +0200

    SumSquareDiffrence - tenNaturalNumbersDone

--- src/main/java/projectEuler/SumSquareDiffrence.java
+++ src/main/java/projectEuler/SumSquareDiffrence.java
@@ -15,26 +15,32 @@ Find the difference between the sum of the squares of the first one hundred natu
 
 public class SumSquareDiffrence {
     public static void main(String[] args) {
-
-        theSumOfTheSquaresOfTheFirstTenNaturalNumbers();
-        theSquareOfTheSumOfTheFirstTenNaturalNumbers();
-        henceTheDifferenceBetweenTheSumOfTheSquaresOfTheFirstTenNaturalNumbers(theSumOfTheSquaresOfTheFirstTenNaturalNumbers(),theSquareOfTheSumOfTheFirstTenNaturalNumbers());
+        tenNaturalNumbers();
     }
 
-    private static int henceTheDifferenceBetweenTheSumOfTheSquaresOfTheFirstTenNaturalNumbers(int theSumOfTheSquaresOfTheFirstTenNaturalNumbers, int theSquareOfTheSumOfTheFirstTenNaturalNumbers) {
-        int result = 0;
+    private static void tenNaturalNumbers() {
+        System.out.println(theSumOfTheSquaresOfTheFirstTenNaturalNumbers());
+        System.out.println(theSquareOfTheSumOfTheFirstTenNaturalNumbers());
+        System.out.println(henceTheDifferenceBetweenTheSumOfTheSquaresOfTheFirstTenNaturalNumbers(theSumOfTheSquaresOfTheFirstTenNaturalNumbers(), theSquareOfTheSumOfTheFirstTenNaturalNumbers()));
+    }
 
+    private static double henceTheDifferenceBetweenTheSumOfTheSquaresOfTheFirstTenNaturalNumbers(double theSumOfTheSquaresOfTheFirstTenNaturalNumbers, double theSquareOfTheSumOfTheFirstTenNaturalNumbers) {
+        double result = theSquareOfTheSumOfTheFirstTenNaturalNumbers - theSumOfTheSquaresOfTheFirstTenNaturalNumbers;
         return result;
     }
 
-    private static int theSquareOfTheSumOfTheFirstTenNaturalNumbers() {
-        int theSquareOfSum = 0;
+    private static double theSquareOfTheSumOfTheFirstTenNaturalNumbers() {
+        int b = 2;
+        double theSquareOfSum = Math.pow((1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10), b);
 
         return theSquareOfSum;
     }
 
-    private static int theSumOfTheSquaresOfTheFirstTenNaturalNumbers() {
-        int theSumOfSquares = 0;
+    //    The sum of the squares of the first ten natural numbers is,
+//    1^2 + 2^2 + ... + 10^2 = 385
+    private static double theSumOfTheSquaresOfTheFirstTenNaturalNumbers() {
+        int b = 2;
+        double theSumOfSquares = Math.pow(1, b) + Math.pow(2, b) + Math.pow(3, b) + Math.pow(4, b) + Math.pow(5, b) + Math.pow(6, b) + Math.pow(7, b) + Math.pow(8, b) + Math.pow(9, b) + Math.pow(10, b);
 
         return theSumOfSquares;
     }
