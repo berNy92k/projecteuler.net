3fd7d8a299577cfd56d848ad8b7976a446041ef3
daw.bernacki@gmail.com <mirradave13>
2018-06-10 03:26:40 +0200

    SumSquareDifference - done

--- src/main/java/projectEuler/SumSquareDiffrence.java
+++ src/main/java/projectEuler/SumSquareDiffrence.java
@@ -16,7 +16,43 @@ Find the difference between the sum of the squares of the first one hundred natu
 public class SumSquareDiffrence {
     public static void main(String[] args) {
         tenNaturalNumbers();
+        System.out.println();
+        hundredNaturalNumbers();
     }
+
+    private static void hundredNaturalNumbers() {
+        System.out.printf("%.0f%n", theSumOfTheSquaresOfTheFirstHundredNaturalNumbers());
+        System.out.printf("%.0f%n", theSquareOfTheSumOfTheFirstHundredNaturalNumbers());
+        System.out.printf("%.0f%n", henceTheDifferenceBetweenTheSumOfTheSquaresOfTheFirstHundredNaturalNumbers(theSumOfTheSquaresOfTheFirstHundredNaturalNumbers(), theSquareOfTheSumOfTheFirstHundredNaturalNumbers()));
+    }
+
+    private static double henceTheDifferenceBetweenTheSumOfTheSquaresOfTheFirstHundredNaturalNumbers(double theSumOfTheSquaresOfTheFirstHundredNaturalNumbers, double theSquareOfTheSumOfTheFirstHundredNaturalNumbers) {
+        double result = theSquareOfTheSumOfTheFirstHundredNaturalNumbers - theSumOfTheSquaresOfTheFirstHundredNaturalNumbers;
+        return result;
+    }
+
+    private static double theSquareOfTheSumOfTheFirstHundredNaturalNumbers() {
+        int b = 2;
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum = sum + i;
+        }
+        double theSquareOfSum = Math.pow((sum), b);
+
+        return theSquareOfSum;
+    }
+
+    private static double theSumOfTheSquaresOfTheFirstHundredNaturalNumbers() {
+        int b = 2;
+        double theSumOfSquares = 0;
+        for (int i = 1; i <= 100; i++) {
+            theSumOfSquares = theSumOfSquares + Math.pow(i, b);
+        }
+
+        return theSumOfSquares;
+    }
+
 
     private static void tenNaturalNumbers() {
         System.out.println(theSumOfTheSquaresOfTheFirstTenNaturalNumbers());
