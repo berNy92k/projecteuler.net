5055259073c20af0803f59de81f202a9f6ef0dcc
daw.bernacki@gmail.com <mirradave13>
2018-06-10 02:55:55 +0200

    SmallestMultiple - done

--- src/main/java/projectEuler/SmallestMultiple.java
+++ src/main/java/projectEuler/SmallestMultiple.java
@@ -17,11 +17,30 @@ public class SmallestMultiple {
         System.out.println("smallestMultipleOf1To20 is: " + smallestMultipleOf1To20());
 
     }
+
+    private static Integer smallestMultipleOf1To10() {
+        int a = 10;
+        Set<Integer> smallestMultipleSet = new LinkedHashSet<>();
+        for (int i = 1; i < 10000; i++) {
+            int isItOk = 0;
+            for (int j = 1; j <= a; j++) {
+                if (i % j == 0) {
+                    isItOk++;
+                }
+            }
+            if (isItOk == a) {
+                smallestMultipleSet.add(i);
+            }
+        }
+        Integer min = Collections.min(smallestMultipleSet);
+        return min;
+    }
+
     private static Integer smallestMultipleOf1To20() {
         Set<Integer> smallestMultipleSet = new LinkedHashSet<>();
         for (int i = 1; i < 1000000000; i++) {
             if (i % 1 == 0 && i % 2 == 0 && i % 3 == 0 && i % 4 == 0 && i % 5 == 0 && i % 6 == 0 && i % 7 == 0 && i % 8 == 0 && i % 9 == 0 && i % 10 == 0 &&
-            i % 11 == 0 && i % 12 == 0 && i % 13 == 0 && i % 14 == 0 && i % 15 == 0 && i % 16 == 0 && i % 17 == 0 && i % 18 == 0 && i % 19 == 0 && i % 20 == 0) {
+                    i % 11 == 0 && i % 12 == 0 && i % 13 == 0 && i % 14 == 0 && i % 15 == 0 && i % 16 == 0 && i % 17 == 0 && i % 18 == 0 && i % 19 == 0 && i % 20 == 0) {
                 smallestMultipleSet.add(i);
             }
         }
@@ -33,14 +52,17 @@ public class SmallestMultiple {
         return min;
     }
 
-    private static Integer smallestMultipleOf1To10() {
-        Set<Integer> smallestMultipleSet = new LinkedHashSet<>();
-        for (int i = 1; i < 10000; i++) {
-                if (i % 1 == 0 && i % 2 == 0 && i % 3 == 0 && i % 4 == 0 && i % 5 == 0 && i % 6 == 0 && i % 7 == 0 && i % 8 == 0 && i % 9 == 0 && i % 10 == 0) {
-                    smallestMultipleSet.add(i);
-                }
-        }
-        Integer min = Collections.min(smallestMultipleSet);
-        return min;
-    }
 }
+
+
+//    private static Integer smallestMultipleOf1To10() {
+//        Set<Integer> smallestMultipleSet = new LinkedHashSet<>();
+//        for (int i = 1; i < 10000; i++) {
+//            if (i % 1 == 0 && i % 2 == 0 && i % 3 == 0 && i % 4 == 0 && i % 5 == 0 && i % 6 == 0 && i % 7 == 0 && i % 8 == 0 && i % 9 == 0 && i % 10 == 0) {
+//                smallestMultipleSet.add(i);
+//            }
+//        }
+//        Integer min = Collections.min(smallestMultipleSet);
+//        return min;
+//    }
+
