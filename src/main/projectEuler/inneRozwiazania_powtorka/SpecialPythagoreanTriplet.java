aa4ad11410dd819c3dc6825a9c3be36f5d02bde5
Dawid Bernacki <daw.bernacki@gmail.com>
2018-12-23 03:07:24 +0100

    Update

--- src/main/projectEuler/inneRozwiazania_powtorka/SpecialPythagoreanTriplet.java
+++ src/main/projectEuler/inneRozwiazania_powtorka/SpecialPythagoreanTriplet.java
@@ -11,7 +11,27 @@ package inneRozwiazania_powtorka;
 public class SpecialPythagoreanTriplet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 1000;
+		findTheProductABC(a);
+
+	}
+
+	private static void findTheProductABC(int a) {
+
+		for (int i = 1; i <= a; i++) {
+			for (int j = 1; j <= a; j++) {
+				for (int k = 1; k <= a; k++) {
+					if (i < j && j < k && (Math.pow(i, 2) + Math.pow(j, 2) == (Math.pow(k, 2)))) {
+						if (i + j + k == 1000) {
+							System.out.println("i = " + i);
+							System.out.println("j = " + j);
+							System.out.println("l = " + k);
+							System.out.println("The product abc: " + i * j * k);
+						}
+					}
+				}
+			}
+		}
 
 	}
 
